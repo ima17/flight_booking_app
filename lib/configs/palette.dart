74ec495f21fcbed877118a1e42d07a6d1418afa3
ima17 <ima.sudaraka@outlook.com>
2023-06-22 23:13:44 +0530

    Create palette

---
+++ lib/configs/palette.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  // Common Colors
+  static const normalTextColor = Color(0xFF363636);
+  static const subtitleTextColor = Color(0xFFACACAC);
+  static const whiteColor = Color(0xFFFFFFFF);
+  
+  
+  // Landing Screen
+  static const darkBlueColor = Color(0xFF00386B);
+  static const greyColor = Color(0xFFB2C7C5);
+  static const lightColor = Color(0xFFF8F6E7);
+  static const themeBlueColor = Color(0xFF0084FF);
+  
+
+  //Results screen
+  static const accentColor = Color(0xFF006ED4);
+  static const lightGreyColor = Color(0xFFF5F6F8);
+  static const rulerColor = Color(0xFFDADADA);
+}
