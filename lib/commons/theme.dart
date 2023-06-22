c60c59d4ed52efd290252739e49643de2353ceb8
ima17 <ima.sudaraka@outlook.com>
2023-06-22 23:29:37 +0530

    Add assets

---
+++ lib/commons/theme.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+
+// final appTheme = ThemeData(
+//   colorScheme: ColorScheme.fromSwatch(
+//     primarySwatch: Colors.red,
+//   ),
+//   textTheme: const TextTheme(
+//     headline1: TextStyle(
+//         fontSize: 72.0, fontWeight: FontWeight.bold, color: Colors.black),
+//     headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+//     bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+//   ),
+//   accentColor: Color(0xFFC3D6FE),
+//   highlightColor: Color(0xFFC3D6FE),
+//   fontFamily: "Manrope",
+//   scaffoldBackgroundColor: Colors.white,
+//   backgroundColor: Colors.white,
+// );
