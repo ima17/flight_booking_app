0b3b539cea815684bd3709e8b3abcdacec5f97b3
ima17 <ima.sudaraka@outlook.com>
2023-06-26 02:18:05 +0530

    Add button widget

---
+++ lib/widgets/button_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flight_booking_app/configs/palette.dart';
+import 'package:flight_booking_app/configs/theme_constants.dart';
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({Key? key, required this.label, required this.onPressed})
+      : super(key: key);
+
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Palette.themeBlueColor,
+          borderRadius: BorderRadius.circular(20.0),
+          // // boxShadow: [
+          // //   BoxShadow(
+          // //     color: Colors.grey.withOpacity(0.25),
+          // //     spreadRadius: 3,
+          // //     blurRadius: 4,
+          // //     offset: const Offset(0, 3),
+          // //   ),
+          // ],
+        ),
+        child: const SizedBox(
+          height: 60.0,
+          width: 350,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Search Flights",
+                style: TextStyle(
+                  color: Palette.whiteColor,
+                  fontSize: 16.0,
+                  fontFamily: ThemeConstants.font,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/landing_screen_bottom_container.dart
+++ lib/widgets/landing_screen_bottom_container.dart
@@ -1,10 +1,12 @@
-import 'package:flight_booking_app/widgets/date_picker_widget.dart';
-import 'package:flight_booking_app/widgets/passenger_luggage_widget.dart';
-import 'package:flight_booking_app/widgets/select_class_widget.dart';
+import 'package:flight_booking_app/screens/results_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../configs/palette.dart';
 import 'trip_selector_widget.dart';
+import 'date_picker_widget.dart';
+import 'passenger_luggage_widget.dart';
+import 'select_class_widget.dart';
+import 'button_widget.dart';
 
 class LandingScreenBottomContainer extends StatelessWidget {
   const LandingScreenBottomContainer({
@@ -26,28 +28,39 @@ class LandingScreenBottomContainer extends StatelessWidget {
                 topRight: Radius.circular(40.0),
               ),
             ),
-            padding: const EdgeInsets.all(35),
-            child: const Column(
-              children: [
-                TripSelectorWidget(),
-                SizedBox(
-                  height: 40,
-                ),
-                DatePickerWidget(),
-                SizedBox(
-                  height: 40,
-                ),
-                PassengerLuggageWidget(),
-                SizedBox(
-                  height: 40,
-                ),
-                SelectClassWidget(),
-              ],
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.fromLTRB(35, 35, 35, 0),
+              child: Column(
+                children: [
+                  const TripSelectorWidget(),
+                  const SizedBox(height: 40),
+                  const DatePickerWidget(),
+                  const SizedBox(height: 40),
+                  const PassengerLuggageWidget(),
+                  const SizedBox(height: 40),
+                  const SelectClassWidget(),
+                  const SizedBox(height: 40),
+                  ButtonWidget(
+                    label: 'Search Fights',
+                    onPressed: () async {
+                      await Navigator.pushNamed(context, ResultScreen.id);
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
-          const Positioned(
+          Positioned(
             top: -40.0,
-            child: Text("One way"),
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "One way",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
         ],
       ),
