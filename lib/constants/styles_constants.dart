cac8fc7e62a733639b5d11f302026cdd98638d88
ima17 <ima.sudaraka@outlook.com>
2023-06-25 22:41:13 +0530

    Add styles constansts

---
+++ lib/constants/styles_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flight_booking_app/configs/theme_constants.dart';
+import 'package:flight_booking_app/configs/palette.dart';
+import 'package:flutter/material.dart';
+
+const kLabelStyle = TextStyle(
+  color: Palette.subtitleTextColor,
+  fontFamily: ThemeConstants.font,
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+);
